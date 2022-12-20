bf01b6ded2038bba6c4b29c56433c008490ab894
LeylaJansson <leylajansson@hotmail.com>
2022-12-20 16:55:24 +0000

    Add stock calculation and updating of the stock worksheet

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ def validate_data(values):
     """
     try:
         [int(value) for value in values]
-        if len(values) != 6:
+        if len(values) != 7:
             raise ValueError(
                 f"Exactly 7 values required, you provided {len(values)}"
             )
@@ -101,6 +101,21 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate average stock for each item +5 extras of each item
+    """
+    print("Calculating stock data\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average + 5
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -110,6 +125,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 main()
